c95493abb8c153013615a279fa14b09353d5fc87
majedalhashdi7-ux <divdhash9@gmail.com>
2026-03-27 18:36:16 +0300

    🚀 نشر تلقائي - ٢٧‏/٣‏/٢٠٢٦، ٦:٣٦:١٦ م

---
+++ client-app/src/app/orders/layout.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import ClientLayout from '../client/layout';
+
+export default function OrdersLayout({ children }: { children: React.ReactNode }) {
+    return <ClientLayout>{children}</ClientLayout>;
+}
